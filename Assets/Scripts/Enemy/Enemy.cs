d808de7f33d38a62759ab7b138266202080ad929
Alwin Grossbach <alwin.grossbach@outlook.de>
2021-03-12 17:44:22 +0100

    adjusted enemy 2d into enemy 3d

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -17,7 +17,7 @@ public enum EnemyState
     returningHome,
     attacking,
     stagger
-   
+
 }
 // DefaultMovementType will determine how the enemy moves when in the walking state.
 public enum DefaultMovementType
@@ -33,10 +33,10 @@ public class Enemy : MonoBehaviour
 {
     [Header("Do not assign values below. They are set as needed or will be assigned by the script.")]
     public EnemyState currentState;
-    public Rigidbody2D enemyRb;
-    Vector2 startPosition;
-    Vector2 movementDirection;
-    bool patrolDirectionPositive = true;   
+    public Rigidbody enemyRb;
+    Vector3 startPosition;
+    Vector3 movementDirection;
+    bool patrolDirectionPositive = true;
     // player is target by default. This will determine who the enemy checking distance from and/or chasing/attacking.
     private Transform target;
 
@@ -46,8 +46,8 @@ public class Enemy : MonoBehaviour
     public float moveSpeed;
     [Tooltip("How far the enemy can move on the x axis from their spawn position. If Patrolling Up/Down, this should be Zero.")]
     public int xRange;
-    [Tooltip("How far the enemy can move on the y axis from their spawn position. If Patrolling Left/Right, this should be Zero.")]
-    public int yRange;
+    [Tooltip("How far the enemy can move on the z axis from their spawn position. If Patrolling Left/Right, this should be Zero.")]
+    public int zRange;
     [Tooltip("How far the Player can be before the enemy will chase them.")]
     public float chaseRadius;
     [Tooltip("How far the Player can be before the enemy will attack them.")]
@@ -56,14 +56,15 @@ public class Enemy : MonoBehaviour
     public string playerTag;
 
 
- 
+
 
 
 
     void Awake()
     {
-        enemyRb = GetComponent<Rigidbody2D>();
-    }   
+        enemyRb = GetComponent<Rigidbody>();
+    }
+
     void Start()
     {
         target = GameObject.FindWithTag(playerTag).transform;
@@ -74,17 +75,19 @@ public class Enemy : MonoBehaviour
         {
             InvokeRepeating("RandomMovementDirection", .1f, (Random.Range(.8f, 2f)));
         }
-    }  
+    }
+
     void Update()
     {
         // Check Distance, and set enemy to chase or attack the player if needed.
-        CheckDistance();        
+        CheckDistance();
         // if not attacking/chasing player, then will check if outside movement range. will set state to ReturningHome and then back to walking once done.
         if (currentState != EnemyState.attacking || currentState != EnemyState.chasingPlayer)
         {
             CheckReturnHome();
         }
     }
+
     void FixedUpdate()
     {
         // process player movement/animation based on currentState
@@ -105,9 +108,11 @@ public class Enemy : MonoBehaviour
             case EnemyState.attacking:
                 AttackCycle();
                 break;
-            default: break;
+            default: 
+                break;
         }
     }
+
     void OnEnable()
     {
         // Reset startPos as enemy should only be renabled when set at a new location. Also ensure Enemy is walking.
@@ -119,6 +124,7 @@ public class Enemy : MonoBehaviour
     {
         startPosition = transform.position;
     }
+
     public void ChangeState(EnemyState newState)
     {
         if (currentState != newState)
@@ -126,34 +132,33 @@ public class Enemy : MonoBehaviour
             currentState = newState;
         }
     }
+
     private void RandomMovementDirection()
     {
         if (currentState == EnemyState.walking)
         {
-            switch (Random.Range(0, 4))
+            switch (Random.Range(0, 3))
             {
                 case 0:
                     // Random Movement in any direction.
-                    movementDirection = new Vector2(RandomNonZero(), RandomNonZero());
+                    movementDirection = new Vector3(RandomNonZero(), 0.5f, RandomNonZero());
                     break;
                 case 1:
                     // Random Movement up or down.
-                    movementDirection = new Vector2(0, RandomNonZero());
+                    movementDirection = new Vector3(0, RandomNonZero(), 0);
                     break;
                 case 2:
                     // Random Movement left or right.
-                    movementDirection = new Vector2(RandomNonZero(), 0);
-                    break;
-                case 3:
-                    // Stand Still.
-                    movementDirection = Vector2.zero;
+                    movementDirection = new Vector3(RandomNonZero(), 0.5f, 0);
                     break;
                 default:
-                    movementDirection = Vector2.zero;
+                    // Stand Still.
+                    movementDirection = Vector3.zero;
                     break;
             }
         }
     }
+
     private int RandomNonZero()
     {
         int randomNum = 0;
@@ -163,6 +168,7 @@ public class Enemy : MonoBehaviour
         }
         return randomNum;
     }
+
     private void CheckDistance()
     {
         // if within Chasing distance but not withing attacking distance, then will chase player.
@@ -178,23 +184,26 @@ public class Enemy : MonoBehaviour
         // else will walk
         else ChangeState(EnemyState.walking);
     }
+
     private float DistanceFromTarget()
     {
-        return Vector2.Distance(target.position, transform.position);
+        return Vector3.Distance(target.position, transform.position);
     }
+
     private void CheckReturnHome()
     {
         // if npc is outside the movementRange set by start position, then will set currentState to returningHome
-        if (currentState == EnemyState.walking && (Mathf.Abs(enemyRb.position.x) > startPosition.x + xRange || Mathf.Abs(enemyRb.position.y) > startPosition.y + yRange))
+        if (currentState == EnemyState.walking && (Mathf.Abs(enemyRb.position.x) > startPosition.x + xRange || Mathf.Abs(enemyRb.position.z) > startPosition.z + zRange))
         {
             ChangeState(EnemyState.returningHome);
         }
         // if npc is returning to start position, and their position is within inner half of movement range, set returnToStartPos to false.
-        else if (currentState == EnemyState.returningHome && Mathf.Abs(enemyRb.position.x) < startPosition.x + (xRange / 2) && Mathf.Abs(enemyRb.position.y) < startPosition.y + (yRange / 2))
+        else if (currentState == EnemyState.returningHome && Mathf.Abs(enemyRb.position.x) < startPosition.x + (xRange / 2) && Mathf.Abs(enemyRb.position.z) < startPosition.z + (zRange / 2))
         {
             ChangeState(EnemyState.walking);
         }
     }
+
     private void MakeNpcWalk()
     {
         switch (movementType)
@@ -204,7 +213,7 @@ public class Enemy : MonoBehaviour
                 break;
 
             case DefaultMovementType.standingStill:
-                MoveEnemy(Vector2.zero);
+                MoveEnemy(Vector3.zero);
                 break;
 
             case DefaultMovementType.patrolXAxis:
@@ -212,36 +221,38 @@ public class Enemy : MonoBehaviour
                 break;
 
             case DefaultMovementType.patrolYAxis:
-                PatrolY();
+                PatrolZ();
                 break;
             default: break;
         }
     }
-    private void PatrolY()
+
+    private void PatrolZ()
     {
         if (patrolDirectionPositive)
         {
-            movementDirection = Vector2.up;
-            if (enemyRb.position.y >= startPosition.y + yRange)
+            movementDirection = Vector3.up;
+            if (enemyRb.position.z >= startPosition.z + zRange)
             {
                 patrolDirectionPositive = false;
             }
         }
         else if (!patrolDirectionPositive)
         {
-            movementDirection = Vector2.down;
-            if (enemyRb.position.y <= startPosition.y - yRange)
+            movementDirection = Vector3.down;
+            if (enemyRb.position.z <= startPosition.z - zRange)
             {
                 patrolDirectionPositive = true;
             }
         }
         MoveEnemy(movementDirection);
     }
+
     private void PatrolX()
     {
         if (patrolDirectionPositive)
         {
-            movementDirection = Vector2.right;
+            movementDirection = Vector3.right;
             if (enemyRb.position.x >= startPosition.x + xRange)
             {
                 patrolDirectionPositive = false;
@@ -249,7 +260,7 @@ public class Enemy : MonoBehaviour
         }
         else if (!patrolDirectionPositive)
         {
-            movementDirection = Vector2.left;
+            movementDirection = Vector3.left;
             if (enemyRb.position.x <= startPosition.x - xRange)
             {
                 patrolDirectionPositive = true;
@@ -257,34 +268,39 @@ public class Enemy : MonoBehaviour
         }
         MoveEnemy(movementDirection);
     }
-    private void MoveEnemy(Vector2 movementDirection)
+
+    private void MoveEnemy(Vector3 movementDirection)
     {
         enemyRb.MovePosition(enemyRb.position + (movementDirection.normalized * moveSpeed * Time.fixedDeltaTime));
     }
+
     private void ChasePlayer()
     {
         Vector3 Direction = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.fixedDeltaTime);
         enemyRb.MovePosition(Direction);
         ChangeState(EnemyState.chasingPlayer);
     }
+
     private void ReturnHome()
     {
         if (movementType == DefaultMovementType.randomMovement)
         {
             //Random.Range used to set target destination to a random spot inside the movement range.
             movementDirection.x = (startPosition.x + Random.Range(-xRange, xRange)) - enemyRb.position.x;
-            movementDirection.y = (startPosition.y + Random.Range(-yRange, yRange)) - enemyRb.position.y;
+            movementDirection.z = (startPosition.z + Random.Range(-zRange, zRange)) - enemyRb.position.z;
         }
         else
         {
             movementDirection.x = (startPosition.x) - enemyRb.position.x;
-            movementDirection.y = (startPosition.y) - enemyRb.position.y;
+            movementDirection.z = (startPosition.z) - enemyRb.position.z;
         }
         MoveEnemy(movementDirection);
     }
+
     public void AttackCycle()
     {
         // Currently will just stand still when attacking.
-        movementDirection = Vector2.zero;
+        movementDirection = Vector3.zero;
     }
+
 }
