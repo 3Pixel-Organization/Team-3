b41f11bf26e0983804fd80caf312dd76c50f43b7
Alwin Grossbach <alwin.grossbach@outlook.de>
2021-04-07 20:20:24 +0200

    Invisible cursor at startup

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -39,6 +39,7 @@ public class GameController : MonoBehaviour
 
     private void InitiateGame()
     {
+        Cursor.visible = false;
         EventBroker.CallInitiateGame(InitialEnemyCount);
     }
 
