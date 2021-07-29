6c960ae4a5feec1440c9aa7b7d5b6a7d86c6e347
camsdono <68128526+camsdono@users.noreply.github.com>
2021-07-29 13:35:02 +0100

    Basic Map and enemy movement system
    Some systems are half complete

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,21 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed;
+    public Transform[] targets;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = 
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
